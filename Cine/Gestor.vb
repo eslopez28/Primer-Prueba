fb5e65a04893050a47753fbc1700daa969d9b855
esteban lopez <eslopez28@hotmail.com>
2014-09-12 21:07:12 -0600

    Agregando Proyecto
    Hasta ahora me di cuenta que no habia copiado los archivos del proyecto,
    sorry ahi

---
+++ Cine/Gestor.vb
@@ -0,0 +1,25 @@
+﻿Public Class Gestor
+
+    Public Function setPelicula(ptituloDist As String, ptituloOrig As String, pidiomaOrig As String, pgenero As String, psubt As String,
+                        porigen As String, pproduccion As String, purl As String, phoras As Integer, pmin As Integer,
+                        pclasificacion As String, pestreno As String, pidResumen As Integer, pidDIrector As Integer)
+
+        Return New MultiPelicula().setPelicua(New Pelicula(ptituloDist, ptituloOrig, pidiomaOrig, psubt, porigen, pgenero, pproduccion, purl, phoras, pmin,
+                                                           pclasificacion, pestreno, 0, pidResumen, pidDIrector))
+    End Function
+
+    Public Function getPeliculas()
+
+        Return New MultiPelicula().getPeliculas()
+    End Function
+
+    Public Function getPeliculaPorNombre(nombre As String)
+
+        Return New MultiPelicula().getPeliculasPorNombre(nombre)
+    End Function
+
+    Public Function getPeliculasPorId(idPelicula As String)
+        Return New MultiPelicula().getPeliculasPorId(idPelicula)
+    End Function
+
+End Class
---
+++ Cine/Resumen.vb
@@ -0,0 +1,29 @@
+﻿Public Class Resumen
+
+    Private idResumen As Integer
+    Private resumenPelicula As String
+    Private listaComentario As ArrayList
+    Public Sub Resumen(pidResumen As Integer, presumenPelicula As String)
+        setIdResumen(pidResumen)
+        setResumenPelicula(presumenPelicula)
+    End Sub
+
+    Public Function setIdResumen(pidResumen As Integer)
+        idResumen = pidResumen
+        Return Nothing
+    End Function
+
+    Public Function getIdResumen() As Integer
+        Return idResumen
+    End Function
+
+    Public Function setResumenPelicula(presumenPelicula As String)
+        resumenPelicula = presumenPelicula
+        Return Nothing
+    End Function
+
+    Public Function getResumenPelicula() As String
+        Return resumenPelicula
+    End Function
+
+End Class
