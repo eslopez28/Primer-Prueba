6a02722dc89b9fad629bc9dad8fbf957738f71cc
esteban lopez <eslopez28@hotmail.com>
2014-10-19 21:44:23 -0600

    Abstracción Figura
    Se realizo la abstracción figura para que sus hijos implementen su
    especificación

--- Figuras/EntitiesLayer/Figura.cs
+++ Figuras/EntitiesLayer/Figura.cs
@@ -8,6 +8,59 @@ namespace EntitiesLayer
 {
     public abstract class Figura
     {
+        private String tipo;
+	private String id;
+	private String color;
+	
+	private static int cantFig = 0;
+	
+	public Figura(String ptipo, String pcolor){
+		setTipo(ptipo);
+		setId();
+		setColor(pcolor);
+	}
 
-    }
+	public String getTipo() {
+		return tipo;
+	}
+
+	private void setTipo(String ptipo) {
+		tipo = ptipo;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	private void setId() {
+		cantFig++;
+		id = "Fig - "+cantFig;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	private void setColor(String pcolor) {
+		color = pcolor;
+	}
+
+	public static int getCantFig() {
+		return cantFig;
+	}
+	
+	public abstract double calcularArea();
+	
+	public abstract double calcularPerimetro();
+	
+	public abstract String dibujar();
+	
+	public String toString(){
+		String estado="";
+		estado += "Tipo: "+getTipo()+"\n";
+		estado += "Id: "+getId()+"\n";
+		estado += "Color: "+getColor()+"\n";
+		
+		return estado;
+	}
 }
